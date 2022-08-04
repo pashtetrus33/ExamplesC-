b2c282441df1a5bc75c3977b9de802e3291cfe75
pashtetrus33@ <pashtetrus@gmail.com>
2022-08-04 21:08:35 +0300

    Example_dog_and_friends

---
+++ Example_dog_and_friends/Program.cs
@@ -0,0 +1,30 @@
+﻿int count = 0;
+double distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+double time = 0;
+
+while (distance > 10)
+{
+
+if (friend == 1)
+{
+    time = (double)distance/(firstFriendSpeed + dogSpeed);
+    friend = 2;
+}
+
+else 
+{
+time = (double)distance/(secondFriendSpeed + dogSpeed);
+    friend = 1;
+}
+
+distance = distance - (firstFriendSpeed + secondFriendSpeed)*time;
+
+count++;
+
+}
+
+Console.WriteLine("Cобака пробежит " + count + " раз.");
